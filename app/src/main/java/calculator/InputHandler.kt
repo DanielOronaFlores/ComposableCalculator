94ceb45ed4457f792e01270cd73bf9325f58b260
DANIEL ALEJANDRO ORONA FLORES <a19300085@live.ceti.mx>
2024-10-29 17:35:44 -0600

    If else logic replaced by when logic

--- app/src/main/java/calculator/InputHandler.kt
+++ app/src/main/java/calculator/InputHandler.kt
@@ -7,38 +7,54 @@ class InputHandler(
     private val displayViewModel: DisplayViewModel,
     private val stack: Stack<String>
 ) {
-    private val arithmeticOperators = listOf("+", "x", "/") // These are going to have a similar behavior
-    private val numbers = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9") // These are going to have a similar behavior
+    private val arithmeticOperators =
+        listOf("+", "x", "/") // These are going to have a similar behavior
+    private val numbers = listOf(
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9"
+    ) // These are going to have a similar behavior
 
     fun handleInput(input: String) {
         println("Input received: $input")
 
-        if (arithmeticOperators.contains(input)) {
-            if (stack.peek() in arithmeticOperators || stack.peek() == "-") {
+        when {
+            arithmeticOperators.contains(input) -> {
+                if (stack.peek() in arithmeticOperators || stack.peek() == "-") {
+                    stack.pop()
+                    deleteLastCharacter()
+                }
+                if (stack.peek() in numbers) {
+                    stack.push(input)
+                    updateText(input)
+                }
+            }
+            input == "<-" -> {
                 stack.pop()
                 deleteLastCharacter()
             }
-            if (stack.peek() in numbers) {
-                stack.push(input)
-                updateText(input)
+            input == "AC" -> {
+                stack.clear()
+                clearDisplay()
             }
-        } else if (input == "<-") {
-            stack.pop()
-            deleteLastCharacter()
-        } else if (input == "AC") {
-            stack.clear()
-            clearDisplay()
-        }
-        else {
-            stack.push(input)
-            updateText(input)
+            else -> {
+                    stack.push(input)
+                    updateText(input)
+            }
         }
 
         println(stack.toString())
     }
 
     private fun updateText(input: String) {
-        displayViewModel.updateText( displayViewModel.text + input)
+        displayViewModel.updateText(displayViewModel.text + input)
     }
 
     private fun deleteLastCharacter() {
